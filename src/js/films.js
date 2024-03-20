4a4b41286e549fbbc9d8383a43bd4e6936be6102
manojadhikari22 <manojadhikari2207@gmail.com>
2024-03-20 10:17:48 +0100

    added images according to the film title

--- src/js/films.js
+++ src/js/films.js
@@ -1,6 +1,18 @@
 
 const filmData = document.querySelector('#starWarsList');
 
+const filmImages = {
+    "A New Hope": "assets/A-new-hope.jpg",
+    "The Empire Strikes Back": "assets/The-empire-strikes-back.jpg",
+    "Return of the Jedi": "assets/Return-of-the-jedi.jpg",
+    "The Phantom Menace": "assets/The-phantom-menache.png",
+    "Attack of the Clones": "assets/Attack-of-the-clones.webp",
+    "Revenge of the Sith": "assets/Revenge-of-the-sith.png",
+    "The Force Awakens": "assets/The-force-awakens.png",
+    "The Last Jedi": "assets/The-last-jedi.webp",
+    "The Rise of Skywalker": "assets/The-rise-of-skywalker.png"
+};
+
 const fetchStarWars = async()=> {
     const response = await fetch('https://swapi.dev/api/films')
     const data = await response.json();
@@ -41,7 +53,14 @@ const renderUsers = (films)=> {
         releaseDate.textContent = `Release Date: ${film.release_date}`;
         openingCrawl.textContent = `Opening Crawl: ${film.opening_crawl}`;
 
-        image.src = `assets/A-new-hope.jpg`;
-        image.alt = `${film.name} Image`;
+         // Set image source based on film title
+         const imageUrl = filmImages[film.title];
+         if (imageUrl) {
+             image.src = imageUrl;
+             image.alt = `${film.title} Image`;
+         }
+
+        //image.src = `assets/A-new-hope.jpg`;
+        //image.alt = `${film.name} Image`;
     });
 }
